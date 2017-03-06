cdeb21eb2955e6be283b48985b97fcd28d2c6ac8
Stephen <sgornick@digicoast.com>
2017-03-06 02:23:26 -0800

    Add support for serving static image of gauge.

--- web_site/dashboard.py
+++ web_site/dashboard.py
@@ -4,11 +4,13 @@ from lxml import html
 from flask import Flask, jsonify, render_template
 from datetime import datetime, timedelta
 import json
+import pytz
 
 
 def request_res_latest_json():
 	# Scrape the latest available measurements from CA DWR CDEC.
 	blank_val = '--'.rjust(10)
+	data = {}
 	url = 'http://cdec.water.ca.gov/cgi-progs/queryF?ORO'
 	req = request.Request(url=url)
 	with request.urlopen(req) as response:
@@ -30,12 +32,41 @@ def request_res_latest_json():
 		datetime.strptime(flow_row[0].xpath('text()')[0], '%m/%d/%Y %H:%M'))
 	outflow = int(flow_row[5].xpath('./text()')[0])
 	inflow = int(flow_row[7].xpath('./text()')[0])
-
-	data = {}
 	data['timestamp_str'] = str(datetime.utcnow())
 	data['res_elev_datetime'] = res_elev_datetime
 	data['res_elev'] = res_elev
 	data['flow_datetime'] = flow_datetime
+	data['inflow'] = inflow
+	data['outflow'] = outflow
+	return data
+
+
+def request_gauges_latest_json():
+	# Scrape the latest available measurements from CA DWR CDEC.
+	blank_val = '--'.rjust(10)
+	data = {}
+	PT = pytz.timezone('America/Los_Angeles')  # Pacific Timezone
+	url = 'http://cdec.water.ca.gov/cgi-progs/queryF?ORO'
+	req = request.Request(url=url)
+	with request.urlopen(req) as response:
+		page = response.read()
+		tree = html.fromstring(page)
+	# There could be multiple tables in the page.
+	# The table with hourly measurements has 14 rows and 15 colums.
+	# The last row may be incomplete (blank values). Ignore these.
+	row = tree.xpath(
+		'//div[@class="content_left_column"]/table[count(tr) = 14]/tr[count(td) = 15][td[2][text()!="{}"] and td[6][text()!="{}"] and td[8][text()!="{}"]]'.format(
+			blank_val, blank_val, blank_val))[-1]
+	row_datetime = datetime.strptime(row[0].xpath('text()')[0], '%m/%d/%Y %H:%M')
+	row_datetime = PT.localize(row_datetime)
+	datetime_str = '{:%B %-d, %Y %-I%P} {}'.format(
+		row_datetime, row_datetime.tzname())
+	res_elev = float(row[1].xpath('text()')[0])
+	outflow = int(row[5].xpath('./text()')[0])
+	inflow = int(row[7].xpath('./text()')[0])
+	data['timestamp_str'] = str(datetime.utcnow())
+	data['datetime'] = datetime_str
+	data['res_elev'] = res_elev
 	data['inflow'] = inflow
 	data['outflow'] = outflow
 	return data
@@ -57,6 +88,22 @@ def res_latest():
 	return data
 
 
+def gauges_latest():
+	site_root = Path(__file__).parent
+	path = Path(site_root, "data", "gaugeslatest.json")
+	data = {}
+	if path.is_file():
+		with path.open("r") as data_file:
+			data = json.load(data_file)
+	# If more than a minute old, get the latest.
+	if 'timestamp_str' not in data or \
+		abs((datetime.utcnow() - datetime.strptime(data['timestamp_str'], '%Y-%m-%d %H:%M:%S.%f')).total_seconds()) >= 60:
+		data = request_gauges_latest_json()
+		with path.open('w') as data_file:
+			json.dump(data, data_file)
+	return data
+
+
 app = Flask(__name__)
 
 
@@ -64,6 +111,16 @@ app = Flask(__name__)
 def index():
 	data = res_latest()
 	return render_template('index.html', data=data)
+
+
+@app.route('/gauges/')
+def gauges():
+	data = gauges_latest()
+	return render_template('gauges.html', data=data)
+
+@app.route('/gaugeslatest/', methods=['GET'])
+def gauges_latest_json():
+	return jsonify(gauges_latest())
 
 
 @app.route('/reslatest/', methods=['GET'])
---
+++ web_site/static/js/gauges.js
@@ -0,0 +1,142 @@
+/* global gauge_data:false LinearGauge:false RadialGauge:false */
+$(function() {
+  function range(start, stop, step) {
+    var a, b;
+    a=[start];
+    b=start;
+    while(b < stop) {
+      b += step;
+      a.push(b);
+    }
+    return a;
+  }
+
+  function ticks(max_val) {
+    // Dynamically adjust tick size to leave gauge readable.
+    var vals, order, val;
+    vals = [
+      {flow: 8, ticks: {major: 1, minor: 0.5}},
+      {flow: 4, ticks: {major: 1, minor: 0.2}},
+      {flow: 2, ticks: {major: 0.5, minor: 0.1}},
+      {flow: 1, ticks: {major: 0.25, minor: 0.05}},
+      {flow: 0, ticks: {major: 0.1, minor: 0.05}}
+    ];
+    order = Math.floor((Math.log(max_val) / Math.LN10) + 0.000000001);
+    for (var i = 0; i < vals.length; i++) {
+      if ((max_val / Math.pow(10, order)) >= vals[i]['flow']) {
+        val = vals[i];
+        break;
+      }
+    }
+    val['ticks']['major'] *= Math.pow(10, order);
+    val['ticks']['minor'] *= Math.pow(10, order);
+    return val['ticks'];
+  }
+
+  function max_flow(inflow_val, outflow_val) {
+    // Dynamically set gauge max to next major tick.
+    var max_val, min_max_val, tick;
+    min_max_val = 20000;  // Gauge max should never be less than this.
+    max_val = Math.max(inflow_val, outflow_val, min_max_val);
+    tick = ticks(max_val)['major'];
+    if ((Math.floor(max_val / tick) * tick) == max_val) {
+      // At a tick mark already, leave as-as.
+      return max_val;
+    }
+    // Next major tick.
+    return ((Math.floor(max_val / tick) + 1) * tick);
+  }
+
+  function format_date_title(date_str) {
+    var d, mon, hrs, ampm;
+    d = new Date(date_str);
+    mon = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'][d.getMonth()];
+    hrs = (d.getHours() + 12) % 12 || 12;
+    ampm = (d.getHours() >= 12) ? 'pm' : 'am';
+    return (mon + ' ' + d.getDate() + ' ' + hrs + ampm);
+  }
+
+  function draw_gauges(data) {
+    var max_flow_val;
+    max_flow_val = max_flow(data['value']['inflow'], data['value']['outflow']);
+    $('#title-date').text(data['title']);
+    new LinearGauge({
+      renderTo: 'level-gauge',
+      width: 180,
+      height: 311,
+      units: 'Feet',
+      minValue: 600,
+      maxValue: 925,
+      exactTicks: true,
+      majorTicks: range(600, 900, 50).concat(925),
+      minorTicks: 10,
+      borderShadowWidth: 0,
+      borders: false,
+      needleSide: 'left',
+      colorBarProgress: '#0099cc',
+      colorBar: '#ffffff',
+      barBeginCircle: false,
+      fontValueSize: 16,
+      highlights: [
+        {
+          from: 850,
+          to: 870,
+          color: 'rgba(255, 255, 0, .75)'
+        }, {
+          from: 870,
+          to: 885,
+          color: 'rgba(255, 127, 0, .75)'
+        }, {
+          from: 885,
+          to: 901,
+          color: 'rgba(200, 0, 0, .75)'
+        }, {
+          from: 901,
+          to: 920,
+          color: 'rgba(100, 100, 100, .75)'
+        }
+      ],
+      value: data['value']['level']
+    }).draw();
+    new RadialGauge({
+      renderTo: 'inflow-gauge',
+      width: 170,
+      height: 170,
+      units: 'cfps',
+      minValue: 0,
+      maxValue: max_flow(data['value']['inflow'], data['value']['outflow']),
+      exactTicks: true,
+      majorTicks: range(0, max_flow_val, ticks(max_flow_val)['major']),
+      minorTicks: ticks(max_flow_val)['minor'],
+      borderShadowWidth: 0,
+      borders: false,
+      valueBoxWidth: 32,
+      valueDec: 0,
+      fontValueSize: 32,
+      highlights: [],
+      value: data['value']['inflow']
+    }).draw();
+    new RadialGauge({
+      renderTo: 'outflow-gauge',
+      width: 170,
+      height: 170,
+      units: 'cfps',
+      minValue: 0,
+      maxValue: max_flow_val,
+      exactTicks: true,
+      majorTicks: range(0, max_flow_val, ticks(max_flow_val)['major']),
+      minorTicks: ticks(max_flow_val)['minor'],
+      borderShadowWidth: 0,
+      borders: false,
+      valueBoxWidth: 32,
+      valueDec: 0,
+      fontValueSize: 32,
+      highlights: [],
+      value: data['value']['outflow']
+    }).draw();
+  }
+
+  jQuery(document).ready(function(){
+    draw_gauges(gauge_data);
+  });
+});
