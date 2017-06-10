faf8f676076242ac46d608c524cb8dca91bebcd8
Stephen <sgornick@digicoast.com>
2017-06-10 03:54:09 -0700

    Add script refreshgauges.sh

---
+++ deploy/scripts/refreshgauges.sh
@@ -0,0 +1,1 @@
+chromium-browser --headless --disable-gpu --screenshot=/var/www/orovilledam.org/web_site/static/img/gauges.png --hide-scrollbars --window-size="600,350" https://orovilledam.org/gauges
